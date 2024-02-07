86ab6da5da7d3278a82662121a53b2d4aeedf9ee
Md Aftab <mdaftabmth22@gmail.com>
2024-02-07 08:46:02 +0530

    Setup hearder section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Header } from "./components";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="w-screen h-auto flex flex-col">
+      <Header />
+    </div>
+  );
+};
 
 export default App;
