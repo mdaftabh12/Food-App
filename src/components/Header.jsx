8d0e56034a9270c3e01ccd1c1eb6bd4d91c4b839
Md Aftab <mdaftabmth22@gmail.com>
2024-02-08 09:01:04 +0530

    Hearder section created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Logo from "./img/logo.png";
 
-const Header = ()=>{
-    return <div>Header</div>
-}
+const Header = () => {
+  return (
+    <header className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+      {/* destop & tablet */}
+      <div className="hidden md:flex w-full h-full ">
+        <div className="flex items-center gap-2">
+          <img src={Logo} className="w-8 object-cover" alt="logo" />
+          <p className="text-headingColor text-xl font-bold">City</p>
+        </div>
+
+        <ul className="flex items-center gap-8 ml-auto">
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Home
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Menu
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            About Us
+          </li>
+          <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            Service
+          </li>
+        </ul>
+      </div>
+      {/* mobile */}
+      <div className="flex md:hidden w-full h-full"></div>
+    </header>
+  );
+};
 
 export default Header;
